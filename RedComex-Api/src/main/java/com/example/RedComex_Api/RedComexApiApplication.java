459175abf531a9d144b7da6c818d0a3e435dfbd5
Heider Restrepo <heiderharz@gmail.com>
2025-06-27 08:32:54 -0500

    Creacion del proyecto

---
+++ RedComex-Api/src/main/java/com/example/RedComex_Api/RedComexApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.RedComex_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedComexApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedComexApiApplication.class, args);
+	}
+
+}
